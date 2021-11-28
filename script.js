d7c6336c57a6d6e3e545a6f91a2ed800302e8c6d
unknown <aly.delvalle.9@gmail.com>
2021-11-29 00:42:45 +0800

    add styling

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const button = document.querySelector('#btn');
 
-// const show = function(data){
-//     console.log({data : data})
-// }
-
 // get joke
 async function getJoke(){
     const joke = document.querySelector('#joke');
-    joke.innerHTML = "Waiting for a joke...";
+    const loadingText = ['This one is the best...', 'Waiting for a joke...', "Here's a good one...", "You're gonna laugh for sure..."]
+    joke.style.backgroundColor = "#69A562";
+    joke.style.color = "whitesmoke";
+    joke.innerHTML = loadingText[Math.floor(Math.random()*4)];
     const getData = await fetch('https://icanhazdadjoke.com/', 
     {headers: {
         Accept: 'application/json'}
     })
     const results = await getData.json(); 
+    // check
     console.log(results.joke)
-
+    // display joke
+    joke.style.backgroundColor = "whitesmoke";
+    joke.style.color = "#C98B35";
     joke.innerHTML = results.joke;
 };
 
 // display joke on click
 button.addEventListener('click', function(){
     getJoke();
-    console.log('working');
+    // console.log('working');
 });
